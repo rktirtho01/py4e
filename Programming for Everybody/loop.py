795c10f40c24fda91b19ce253463b821f1baf33a
RK Tirtho <rktirtho@gmail.com>
2020-07-19 23:14:02 +0600

    Loop in pytho

---
+++ Programming for Everybody/loop.py
@@ -0,0 +1,16 @@
+products = ['SmartPhone', 'TV', 'Camera', 'Light', 'Fan']
+
+for i in products:
+    print(i)
+
+for j in range(3):
+    for k in range(3):
+        print(j,k)
+
+for l in range(5):
+    for m in range(l):
+        print(l)
+counter = 0;
+while counter is not 3:
+    print(counter)
+    counter = int(input(counter))

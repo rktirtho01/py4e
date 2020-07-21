50851e4597c0784d6ee322033f457f355f51979b
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-07-21 23:18:26 +0600

    File reader and startWith function'

--- Programming for Everybody/file.py
+++ Programming for Everybody/file.py
@@ -1,6 +1,8 @@
 fhand = open('mbox-short.txt')
 
 count = 0 
-for line in fhand: 
-   print(line.rstrip())
+for line in fhand:
+   if not line.startswith('From'): 
+      continue 
+   print(line)
 

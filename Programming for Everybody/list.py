5f4485c48122f4175cf3d86641e0d7cf618ff626
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-07-23 21:01:59 +0600

    list extends

--- Programming for Everybody/list.py
+++ Programming for Everybody/list.py
@@ -3,8 +3,21 @@ names = list()
 names = ['Rejaul', 'Siam']
 names.append('Hasib')
 names.append('Kamal')
+# List is muteable
+print(names[1])
 
+# Sorting list
 names.sort()
+
 
 for i in names:
     print(i)
+
+print('List sclicing')
+
+scliced= names[0:2]
+
+print(scliced)
+
+names.extend(["Mahin", "kamal"])
+print(names)

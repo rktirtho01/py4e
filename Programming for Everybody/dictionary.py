618ffbcb2a74377dd6090caf022211252e14c7c9
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-07-24 01:04:59 +0600

    "tuples file added"

--- Programming for Everybody/dictionary.py
+++ Programming for Everybody/dictionary.py
@@ -1,16 +1,1 @@
-movies = {"Tom cruse":"Missin Impossible", "Delian Criz": "Jems Bond" }
-barChart = dict();
-
-print(movies)
-
-fhand = open("mbox-short.txt")
-
-for line in fhand:
-    for w in line:
-        if w not in barChart:
-            barChart[w] = 1
-        else:
-            barChart[w] = barChart[w]+1
-
-for i in barChart:
-    print(i, barChart[i])
+print("Hello Word")

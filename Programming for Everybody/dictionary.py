5eec0acf9095a2684e70bc31215a401844fbf07d
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-07-25 21:28:46 +0600

    Dictionary sorting

--- Programming for Everybody/dictionary.py
+++ Programming for Everybody/dictionary.py
@@ -1,4 +1,17 @@
-print("Hello Word")
 
-myDictionary = {"userName":"rktirtho@gmail.com", "password":"kflkjdf9"}
-print(myDictionary)
+fHand = open("romeo.txt")
+
+wordChart = dict()
+for line in fHand:
+    words = line.split()
+    for word in words:
+        wordChart[word] = wordChart.get(word,0)+1
+
+wordsList = list(wordChart)
+wordsList.sort()
+
+for i in wordsList:
+    print(i, wordChart[i])
+
+print(wordsList)
+

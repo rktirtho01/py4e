25a432c554b639845b93b47cb9e1da04d59ed312
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-07-27 23:27:14 +0600

    Gready and non gready expression

--- Using Python to Access Web Data/regular.py
+++ Using Python to Access Web Data/regular.py
@@ -1,8 +1,17 @@
 import re
 
-fHand = open("note.txt")
+fHand = open("mbox-short.txt")
 
 for line in fHand:
     line = line.rstrip()
-    if re.search("F.r", line):
+    if re.search("^X-\S+:", line):
         print(line)
+    if re.search('^F.*?:',line):
+        print(line)
+
+massage = "My favourite number is 2 and 5, 7,17"
+n = re.findall("[0-9]+", massage)
+print(n)
+
+vowel = re.findall('[aeiou]', massage)
+print(vowel)

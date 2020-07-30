23065ab637b23c2cb00ff12b702c383b7ef16886
Rejaul Karim <tirtho@gmail.com>
2020-07-30 18:53:03 +0600

    #4 Grammatical mistake resolved

--- Programming for Everybody/dictionary.py
+++ Programming for Everybody/dictionary.py
@@ -1,15 +1,13 @@
 
 fHand = open("romeo.txt")
-"""Read data from text romeo.txt file. split all word from line and make a bar chart using dictionary"""
+"""Reading data from romeo.txt file and spliting all words from each line and make a bar chart using dictionary"""
 wordChart = dict()
 for line in fHand:
     words = line.split()
     for word in words:
         wordChart[word] = wordChart.get(word,0)+1
-
 wordsList = list(wordChart)
 wordsList.sort()
-
 for i in wordsList:
     print(i, wordChart[i])
 
